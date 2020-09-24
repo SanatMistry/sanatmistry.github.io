338cc0d0ff1581ddfda2d0c6db54ed736f23ed29
Sanat Mistry <sanatmistry@gmail.com>
2020-09-24 16:15:19 +0530

    update ui with nav menu

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 function onPageLoad() {
-    setTimeout(() => {
-        loadPage()
-    }, 3500);
+    // setTimeout(() => {
+    //     loadPage()
+    // }, 3500);
 }
 
 function loadPage() {
@@ -17,4 +17,47 @@ function moveCursor(e) {
         $('#cursor').css({top: e.clientY + 'px'})
         $('#cursor').css({left: e.clientX + 'px'})
     }, 200);
+}
+
+function navigateHome() {
+    jQuery('html,body').animate({scrollTop: 0}, 'fast');
+    jQuery('.nav-item').removeClass('nav-item-selected');
+    jQuery('#home-nav').addClass('nav-item-selected');
+}
+
+function navigateProfile() {
+    jQuery('html,body').animate({scrollTop: jQuery("#profile").offset().top - 75}, 'fast');
+    jQuery('.nav-item').removeClass('nav-item-selected');
+    jQuery('#profile-nav').addClass('nav-item-selected');
+}
+
+function navigateSkills() {
+    jQuery('html,body').animate({scrollTop: jQuery('#skills').offset().top - 75}, 'fast');
+    jQuery('.nav-item').removeClass('nav-item-selected');
+    jQuery('#skills-nav').addClass('nav-item-selected');
+}
+function navigateExp() {
+    jQuery('html,body').animate({scrollTop: jQuery('#experience').offset().top - 75}, 'fast');
+    jQuery('.nav-item').removeClass('nav-item-selected');
+    jQuery('#exp-nav').addClass('nav-item-selected');
+}
+function navigateProjects() {
+    jQuery('html,body').animate({scrollTop: jQuery('#projects').offset().top - 75}, 'fast');
+    jQuery('.nav-item').removeClass('nav-item-selected');
+    jQuery('#projects-nav').addClass('nav-item-selected');
+}
+function navigateHireMe() {
+    jQuery('html,body').animate({scrollTop: jQuery('#hireMe').offset().top - 75}, 'fast');
+    jQuery('.nav-item').removeClass('nav-item-selected');
+    jQuery('#comtact-nav').addClass('nav-item-selected');
+}
+
+function openMenu() {
+    jQuery(".navigation-menu-btn").hide();
+    jQuery(".navigation-menu").css('display', 'flex');
+}
+
+function closeMenu() {
+    jQuery(".navigation-menu-btn").show();
+    jQuery(".navigation-menu").hide();
 }
